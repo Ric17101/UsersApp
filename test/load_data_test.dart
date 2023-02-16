84b375e8cabefcccc926c3a2d548f38c99cf7818
Richard <ric17101raguine@gmail.com>
2023-02-16 20:45:49 +0800

    chore: add simple test with bloc_test package

---
+++ test/load_data_test.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:usersapp/api/services.dart';
+import 'package:usersapp/bloc/bloc.dart';
+import 'package:usersapp/bloc/events.dart';
+import 'package:usersapp/bloc/state.dart';
+
+void main() async {
+  group('UserBloc', () {
+    blocTest(
+      'emits [] when nothing is fetched',
+      build: () => UserBloc(UserServices()),
+      expect: () => [],
+    );
+
+    blocTest(
+      'emits [1] when CounterIncrementPressed is added',
+      build: () => UserBloc(UserServices()),
+      act: (bloc) => bloc.add(UserRequested()),
+      expect: () => [isA<UserLoadInProgress>()],
+    );
+  });
+}
